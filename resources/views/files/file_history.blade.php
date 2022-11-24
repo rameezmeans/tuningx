919d0c23e8aba1ee9353cf14da53258db4550e79
Rameez Bhatti <xrkalix@gmail.com>
2022-11-24 18:37:35 +0500

    file history status change

--- resources/views/files/file_history.blade.php
+++ resources/views/files/file_history.blade.php
@@ -13,7 +13,8 @@
                             <th></th>
                             <th>Vehicle</th>
                             <th>Status</th>
-                            <th class="hide-on-small-and-down">Credits</th>                                                                     <th class="hide-on-small-and-down">VIN</th>
+                            <th class="hide-on-small-and-down">Credits</th>
+                            <th class="hide-on-small-and-down">VIN</th>
                             <th>Customer</th>
                             <th>Plate</th>
                             <th class="hide-on-small-and-down"></th>
@@ -21,27 +22,27 @@
                     </thead>
                     <tbody>
                         @foreach($files as $file)
-                        
-                            <tr class="redirect-click" href="#" data-redirect="{{route('file', $file->id)}}">
-                                <td class="clickable-row">
-                                    
-                                    <img alt="" class="img-circle-car-history" src="{{ get_image_from_brand($file->brand) }}">
-                              
-                                    <span class="brand-name">{{$file->brand}}</span>
-                                </td>
-                                <td class="clickable-row">
-                                    {{$file->vehicle()->Name}} {{ $file->engine }} {{ $file->vehicle()->TORQUE_standard }}
-                                </td>
-                                <td class="clickable-row">
-                                    <span class="label label-green">{{$file->status}}<i class="fa fa-check"></i></span></a>
-                                </td>
-                                <td class="clickable-row"><span class="label label-black">{{$file->credits}}</span></td>
-                                <td class="clickable-row"><input class="vehicle-id-input" id="125091" value="{{$file->vin_number}}" readonly="" onclick="copyVin(125091)"></td>
-                                <td class="clickable-row">{{$file->name}}</td>
-                                <td class="clickable-row">{{$file->license_plate}}</td>
-                                <td class="clickable-row">{{$file->created_at->diffForHumans();}}</td>
-                            </tr>
-                        
+
+                        <tr class="redirect-click" href="#" data-redirect="{{route('file', $file->id)}}">
+                            <td class="clickable-row">
+
+                                <img alt="" class="img-circle-car-history" src="{{ get_image_from_brand($file->brand) }}">
+
+                                <span class="brand-name">{{$file->brand}}</span>
+                            </td>
+                            <td class="clickable-row">
+                                {{$file->vehicle()->Name}} {{ $file->engine }} {{ $file->vehicle()->TORQUE_standard }}
+                            </td>
+                            <td class="clickable-row">
+                                <span class="label @if($file->status == 'submitted' || $file->status == 'accepted') label-green @else label-red @endif">{{$file->status}}<i class="fa @if($file->status == 'submitted' || $file->status == 'accepted') fa-check @else fa-close @endif "></i></span></a>
+                            </td>
+                            <td class="clickable-row"><span class="label label-black">{{$file->credits}}</span></td>
+                            <td class="clickable-row"><input class="vehicle-id-input" id="125091" value="{{$file->vin_number}}" readonly="" onclick="copyVin(125091)"></td>
+                            <td class="clickable-row">{{$file->name}}</td>
+                            <td class="clickable-row">{{$file->license_plate}}</td>
+                            <td class="clickable-row">{{$file->created_at->diffForHumans();}}</td>
+                        </tr>
+
                         @endforeach
                     </tbody>
                 </table>
@@ -54,14 +55,15 @@
 @section('pagespecificscripts')
 
 <script type="text/javascript">
-
-    $( document ).ready(function(event) {
+    $(document).ready(function(event) {
         $('.redirect-click').click(function() {
             window.location.href = $(this).data('redirect');
             return false;
         });
 
-        $('table').DataTable({"ordering": false,});
+        $('table').DataTable({
+            "ordering": false,
+        });
     });
 
 </script>
